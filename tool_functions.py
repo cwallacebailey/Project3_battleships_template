1672377a17d52a6857663bcb9d542cc1c5ffaed6
cwallacebailey <cwallacebailey@gmail.com>
2022-03-16 12:42:24 +0000

    create display_clear() to clear terminal when called

---
+++ tool_functions.py
@@ -0,0 +1,13 @@
+import os, sys, time
+
+def display_clear():
+    # This is the URL that this code was found on:
+    # http://www.delftstack.com/howto/python/python-clear-console/
+    """
+    clears console
+    """
+    command = 'clear'
+    if os.name in (
+        'nt', 'dos'):
+            command = 'cls'
+    os.system(command)
