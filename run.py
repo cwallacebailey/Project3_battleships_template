a50b4a967b59bb57d5666da449dc999921cfe109
cwallacebailey <cwallacebailey@gmail.com>
2022-03-16 12:53:56 +0000

    add function check_hit_or_miss() to see if coordinate has ship on it

--- run.py
+++ run.py
@@ -18,3 +18,42 @@ def take_guess(self):
         self.row = int(self.row) - 1
         self.col = int(self.col) - 1
         self.check_guess(self.row, self.col, self.guess_board)
+
+def check_guess(self, row, col, board):
+        """
+        Checks if a ship is at the guess location
+        """
+        print("checking guess")
+        if board[row][col] == "\U0001f30a":
+            display_clear()
+            print("We've shot there before, try another coordinate")
+            self.take_guess()
+        elif board[row][col] == "\U0001f525":
+            display_clear()
+            print("We've shot there before, try another coordinate")
+            self.take_guess()
+        else: 
+            self.check_hit_or_miss(self.row, self.col, self.computer_board, self.guess_board)
+
+def check_hit_or_miss(self, row, col, board, update_board):
+        """
+        Checks if a ship is at the user guess coordinates
+        """
+        display_clear()
+        if board[row][col] == "\U0001f6a2":
+            update_board[row][col] = "\U0001f525"
+            print("direct hit, well done")
+
+            self.computer_ships -= 1
+            
+            if self.computer_ships == 0:
+                print("Its Over")
+                time.sleep(5)
+                self.game_end_win()
+
+            else:
+                self.computer_guess()
+        else:
+            update_board[row][col] = "\U0001f30a"
+            print("Miss")
+            self.computer_guess()
