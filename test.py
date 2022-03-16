c903a1adb462f99fc5b045999b8927caa599e57f
cwallacebailey <cwallacebailey@gmail.com>
2022-03-16 13:13:47 +0000

    create method ship_direction to choose if vertical or horizontal ship

---
+++ test.py
@@ -0,0 +1,24 @@
+import random, time, sys
+from com.tool_functions import *
+from com.board import *
+
+a = " "+chr(9291)
+
+class ship_placement:
+
+    def __init__(self):
+        self.ships = [4, chr(8517), 3, chr(8486), 3, chr(8492), 2, chr(8497)]
+        self.number_of_ships = int(len(self.ships)/2)
+        self.x = 0
+        self.y = 1
+        self.board_size = 8
+        self.array = []
+        self.board = [[chr(128911) for _ in range(self.board_size)] for _ in range(self.board_size)]
+        self.run = self.run()
+
+    def ship_direction(self):
+        if random.randint(0, 1) == 0:
+            direction = True #vertical row + 1
+        else:
+            direction = False #horizontal col + 1
+        return direction
