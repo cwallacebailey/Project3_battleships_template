a1562aca93e7156f095df8c0bf1b845c35cfdbb0
cwallacebailey <cwallacebailey@gmail.com>
2022-03-20 17:29:41 +0000

    begin creating ship damage system, ship_check()

--- test.py
+++ test.py
@@ -1,10 +1,36 @@
 """ this is where we test """
 
+from board import CreateBoard
+
+GO = CreateBoard().board
+
 SHIPS = [chr(8517), chr(8486), chr(8492), chr(8497)]
 
-NUM_SHIPS = len(SHIPS)
+"""
+TEST_ARRAY = [['.'], ['.']]
+print(TEST_ARRAY)
+DOT = ['.'] in TEST_ARRAY
+print(DOT)
+"""
 
-print(NUM_SHIPS)
 
-destroyer = chr(8517) in SHIPS
-print(destroyer)
+def ship_check():
+    """
+    checks if ships present
+    """
+    destroyer = 0
+    battleship = 0
+    ship = 0
+    friggot = 0
+    for row in GO:
+        for column in row:
+            destroyer += 1 if column == chr(8517) else False
+            battleship += 1 if column == chr(8486) else False
+            ship += 1 if column == chr(8492) else False
+            friggot += 1 if column == chr(8497) else False
+
+    print(destroyer, battleship, ship, friggot)
+    return(destroyer, battleship, ship, friggot)
+
+
+ship_check()
