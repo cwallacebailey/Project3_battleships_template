2249c4a42e2972970d98059d2dc3e74e52929d86
cwallacebailey <cwallacebailey@gmail.com>
2022-03-20 18:19:47 +0000

    create system to only action once if each ship destroyed

--- test.py
+++ test.py
@@ -4,33 +4,59 @@ from board import CreateBoard
 
 GO = CreateBoard().board
 
-SHIPS = [chr(8517), chr(8486), chr(8492), chr(8497)]
-
-"""
-TEST_ARRAY = [['.'], ['.']]
-print(TEST_ARRAY)
-DOT = ['.'] in TEST_ARRAY
-print(DOT)
-"""
+destroyer_destroyed = False
+battleship_destroyed = False
+ship_destroyed = False
+friggot_destroyed = False
 
 
-def ship_check():
+class ShipDamage():
     """
-    checks if ships present
+    checks ships damage and reacts if 
+    one of them is destroyed
     """
-    destroyer = 0
-    battleship = 0
-    ship = 0
-    friggot = 0
-    for row in GO:
-        for column in row:
-            destroyer += 1 if column == chr(8517) else False
-            battleship += 1 if column == chr(8486) else False
-            ship += 1 if column == chr(8492) else False
-            friggot += 1 if column == chr(8497) else False
 
-    print(destroyer, battleship, ship, friggot)
-    return(destroyer, battleship, ship, friggot)
+    def __init__(self):
+        self.destroyer = 0
+        self.battleship = 0
+        self.ship = 0
+        self.friggot = 0
+        self.ship_checks = self.ship_check()
+
+    def ship_check(self):
+        """
+        checks if ships present
+        """
+        for row in GO:
+            for column in row:
+                if column == chr(8517):
+                    self.destroyer += 1
+                if column == chr(8486):
+                    self.battleship += 1
+                if column == chr(8492):
+                    self.ship += 1
+                if column == chr(8497):
+                    self.friggot += 1
+        return(self.destroyer, self.battleship, self.ship, self.friggot)
+
+    def once(self, destroyer, battleship, ship, friggot):
+        """
+        ensures that each ship
+        can only be destroyed
+        once
+        """
+        if destroyer == 0 and destroyer_destroyed is False:
+            print("The destroyer's been destroyed")
+            destroyer_destroyed = True
+        if battleship == 0 and battleship_destroyed is False:
+            print("That's the battleship gone")
+            battleship_destroyed = True
+        if ship == 0 and ship_destroyed is False:
+            print("That's the battleship gone")
+            ship_destroyed = True
+        if friggot == 0 and friggot_destroyed is False:
+            print("That's the friggot down")
+            friggot_destroyed = True
 
 
-ship_check()
+ShipDamage()
